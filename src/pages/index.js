8d61ef281cd8b062e9c2ffe003d0bcf83f53ba18
Kareem Osman <kareem@contactza.eu>
2023-05-21 13:21:49 +0300

    changes to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import styles from "@/styles/Home.module.css";
 import Layout from "@/components/Layout";
 import profilePic from "../../public/images/profile/developer-pic-1.png";
 import AnimatedText from "@/components/AnimatedText";
+import Link from "next/link";
+import { LinkArrow } from "@/components/Icons";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,12 +28,28 @@ export default function Home() {
                 text="Turning Vision Into Reality With Code And Design."
                 className="!text-6xl !text-left"
               />
-              <p>
+              <p className="my-4 text-base font-medium">
                 As a skilled full-stack developer, I am dedicated to turning
                 ideas into innovative web applications. Explore my latest
                 projects and articles, showcasing my expertise in React.js and
                 web development.
               </p>
+              <div className="flex items-center self-start mt-2">
+                <Link
+                  href="/resume.pdf"
+                  target={"_blank"}
+                  download={true}
+                  className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border border-solid border-transparent hover:border-dark  "
+                >
+                  Resume <LinkArrow className={"w-6 ml-1"}></LinkArrow>
+                </Link>
+                <Link
+                  href="mailto:test@test.com"
+                  className="ml-4 text-lg font-medium capitalize textdark underline"
+                >
+                  Contact
+                </Link>
+              </div>
             </div>
           </div>
         </Layout>
