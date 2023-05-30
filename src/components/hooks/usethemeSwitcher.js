d0b20a3801e70fc891cfda7e7304491a8af2dc18
Kareem Osman <kareem@contactza.eu>
2023-05-31 00:10:56 +0300

    adding dark theme

---
+++ src/components/hooks/usethemeSwitcher.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+
+const useThemeSwitcher = () => {
+  const preferDarkQuery = "(prefer-color-scheme: dark)";
+  const [mode, setMode] = useState("");
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(preferDarkQuery);
+    const usePref = window.localStorage.getItem("theme");
+
+    const handleChange = () => {
+      if (usePref) {
+        let check = userPref === "dark" ? "dark" : "light";
+        setMode(check);
+        if (check === "dark") {
+          document.documentElement.classList.add("dark");
+        } else {
+          document.documentElement.classList.remove("dark");
+        }
+      } else {
+        let check = mediaQuery.matches ? "dark" : "light";
+        setMode(check);
+
+        if (check === "dark") {
+          document.documentElement.classList.add("dark");
+        } else {
+          document.documentElement.classList.remove("dark");
+        }
+      }
+    };
+
+    mediaQuery.addEventListener("change", handleChange);
+
+    return () => mediaQuery.removeEventListener("change", handleChange);
+  }, []);
+};
+
+export default useThemeSwitcher;
