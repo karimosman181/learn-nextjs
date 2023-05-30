1cc97b3429bec48ccc9df278481d74d4c8c6bb3e
Kareem Osman <kareem@contactza.eu>
2023-05-30 23:59:23 +0300

    add projects page

---
+++ src/pages/projects.js
@@ -0,0 +1,149 @@
+import AnimatedText from "@/components/AnimatedText";
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import { GithubIcon } from "@/components/Icons";
+import project1 from "../../public/images/projects/crypto-screener-cover-image.jpg";
+import project2 from "../../public/images/projects/portfolio-cover-image.jpg";
+
+const FeaturedProject = ({ type, title, summary, img, link, github }) => {
+  return (
+    <article className=" w-full flex  items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-10 relative ">
+      <div className="absolute top-0 -right-3 -z-10 w-[100.8%] h-[103%] rounded-[2rem] bg-dark" />
+      <Link
+        href={link}
+        target="_blank"
+        className=" w-1/2 cursor-pointer overflow-hidden rounded-lg"
+      >
+        <Image src={img} alt={title} className="w-full h-auto" />
+      </Link>
+      <div className=" w-1/2 flex flex-col items-start justify-between pl-6">
+        <span className="text-primary font-medium text-xl"> {type} </span>
+        <Link
+          href={link}
+          target="_blank"
+          className="hover:underline underline-offset-2"
+        >
+          <h2 className=" my-2 w-full text-left text-4xl font-bold">{title}</h2>
+        </Link>
+        <p className="my-2 font-medium text-dark">{summary}</p>
+        <div className="mt-2 flex items-center">
+          <Link href={github} target="_blank" className="w-10">
+            <GithubIcon />
+          </Link>
+          <Link
+            href={link}
+            target="_blank"
+            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibod"
+          >
+            Visit Project
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+const Project = ({ type, title, img, link, github }) => {
+  return (
+    <article className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative">
+      <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark" />
+      <Link
+        href={link}
+        target="_blank"
+        className=" w-full cursor-pointer overflow-hidden rounded-lg"
+      >
+        <Image src={img} alt={title} className="w-full h-auto" />
+      </Link>
+      <div className=" w-full flex flex-col items-start justify-between mt-4 ">
+        <span className="text-primary font-medium text-xl"> {type} </span>
+        <Link
+          href={link}
+          target="_blank"
+          className="hover:underline underline-offset-2"
+        >
+          <h2 className=" my-2 w-full text-left text-3xl font-bold">{title}</h2>
+        </Link>
+
+        <div className="w-full mt-2 flex items-center justify-between">
+          <Link
+            href={link}
+            target="_blank"
+            className="text-lg font-semibold underline"
+          >
+            Visit
+          </Link>
+
+          <Link href={github} target="_blank" className="w-8">
+            <GithubIcon />
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+};
+const Projects = () => {
+  return (
+    <>
+      <Head>
+        <title></title>
+        <meta></meta>
+      </Head>
+      <main className="w-full mb-16 flex flex-col items-center justify-center">
+        <Layout className="pt-16">
+          <AnimatedText
+            text="Imagination Trumps Knowledge!"
+            className="mb-16"
+          />
+          <div className="grid grid-cols-12 gap-24">
+            <div className="col-span-12">
+              <FeaturedProject
+                type="Featured Project"
+                title="Crypto Screener Application"
+                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
+It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
+local currency."
+                img={project1}
+                link=""
+                github=""
+              />
+            </div>
+            <div className="col-span-6">
+              <Project
+                type=""
+                title="React Portfolio Website"
+                img={project2}
+                link=""
+                github=""
+              />
+            </div>
+            <div className="col-span-6">
+              <Project
+                type=""
+                title="Crypto Screener Application"
+                img={project1}
+                link=""
+                github=""
+              />
+            </div>
+            <div className="col-span-12">
+              <FeaturedProject
+                type=""
+                title="React Portfolio Website"
+                summary="A professional portfolio website using React JS, Framer-motion, and Styled-components. It has smooth 
+page transitions, cool background effects, unique design and it is mobile responsive."
+                img={project2}
+                link=""
+                github=""
+              />
+            </div>
+          </div>
+        </Layout>
+      </main>
+    </>
+  );
+};
+
+export default Projects;
